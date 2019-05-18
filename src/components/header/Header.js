6ec3a197036c5398b1a2c4cab3c6e20a9c0a60d6
Sudheer Panda <sudheer.pjava@gmail.com>
2019-05-18 15:57:40 +0530

    refactoring

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1 className="title titleColoer">PhotClicks</h1>
+                <h1 className="title titleColor">PhotClicks</h1>
             </div>
         );
     }
